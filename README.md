085d00c08111e3aa0215a7ed03194fc37d6daf95
Naburi <58636313+Naburi@users.noreply.github.com>
2022-05-31 00:34:25 +0300

    [Add] gitignore

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 please help me
 # how to compile
 cd build && cmake .. && make && ./diploma
+
+# на подумать
+1) Подумать формат над видом global_matrix [пока двумерный вектор]
--- main.cpp
+++ main.cpp
@@ -9,58 +9,12 @@ int main(int argc, char **argv) {
     Model model;
     model.create();
 
-
-    std::vector<std::size_t> nodeTags;
-    std::vector<double> nodeCoords, nodeParams;
-    gmsh::model::mesh::getNodes(nodeTags, nodeCoords, nodeParams);
-
-    std::vector<int> elemTypes;
-    std::vector<std::vector<std::size_t> > elemTags, elemNodeTags;
-    gmsh::model::mesh::getElements(elemTypes, elemTags, elemNodeTags);
-
-    std::cout << "   NodeTags   " << "\n";
-    for (auto i : nodeTags) {
-        std::cout << i << " ";
-    }
-    std::cout << "\n" << "   nodeCoords   " << "\n";
-    int j = 0;
-    for (auto i : nodeCoords) {
-        std::cout  << i << " ";
-        j++;
-        if (j == 3) {
-            std::cout << "\n";
-            j = 0;
-        }
-    }
-
-    std::cout << "\n" << "   nodeParams   " << "\n";
-    for (auto i : nodeParams) {
-        std::cout << i << " ";
-    }
-
-    std::cout << "\n" << "   elementTypes   " << "\n";
-    for (auto i : elemTypes) {
-        std::cout << i << " ";
-    }
-
-    std::cout << "\n" << "   elemTags   " << "\n";
-    for (auto i : elemTags) {
-        for (auto ii: i) {
-            std::cout << ii << " ";
-        }
-        std::cout << "\n";
-    }
-
-
     std::set<std::string> args(argv, argv + argc);
     if(!args.count("-nopopup")) gmsh::fltk::run();
 
     gmsh::finalize();
 
-
-  return 0;
-
-
+    return 0;
 }
 
 
